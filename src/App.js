8bad55161d26417af6073b90d255b0165b80dfd3
luis <luis@betabrand.com>
2021-11-04 23:15:51 -0700

    finalize app structure, adding basic styles

--- src/App.js
+++ src/App.js
@@ -1,117 +1,117 @@
 import './App.css';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import ReactMapboxGl from 'react-mapbox-gl';
 import DrawControl from 'react-mapbox-gl-draw';
 import Geocoder from 'react-mapbox-gl-geocoder';
-
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 
-
 const accessToken = 'pk.eyJ1IjoibHVnYXJ6YSIsImEiOiJja3ZpcTB4MnQyY3hnMzBwMXpvY2hsMDJnIn0.91hW0Z_WIsZNywLN6mEEFw';
+
 const Map = ReactMapboxGl({
   accessToken,
   logoPosition: 'top-right',
 });
 
-
 function App() {
-  const map = useRef(null);
-
   const [nominalPower, setNominalPower] = useState(0);
-  const [lng, setLng] = useState(-155.9794816);
-  const [lat, setLat] = useState(19.6099322);
+  const [center, setCenter] = useState([-155.9794816, 19.6099322]);
   const [zoom, setZoom] = useState(19);
   const [geoCode, setGeoCode] = useState('');
-
+  const [hasDrawn, setHasDrawn] = useState(false)
 
   const handleDraw = ({ features }) => {
-    /** MapBox GL Draw features includes coordinates for drawn
-     * polygon. using `calculateArea` from (https://stackoverflow.com/questions/62323834/calculate-polygon-area-javascript)
+    /** MapBox GL Draw `features` includes coordinates 
+     * for drawn polygon. using `calculateArea` 
+     * from (https://stackoverflow.com/questions/62323834/calculate-polygon-area-javascript)
      * to calculate area. Area multiplied by multiplyer to simulate nominal power calculation.
-     * 
-     * 
      */
 
-    const multiplyer = 12000000000;
+    const coords = features[0].geometry.coordinates[0];
+    let area = 0;
 
-    function calculateArea(coords) {
-      let area = 0;
+    for (let i = 0; i < coords.length; i++) {
+      const [x1, y1] = coords[i];
+      const [x2, y2] = coords[(i + 1) % coords.length];
 
-      for (let i = 0; i < coords.length; i++) {
-        const [x1, y1] = coords[i];
-        const [x2, y2] = coords[(i + 1) % coords.length];
-
-        area += x1 * y2 - x2 * y1
-      }
-
-      return Math.abs(area) / 2;
+      area += x1 * y2 - x2 * y1
     }
 
-    const area = calculateArea(features[0].geometry.coordinates[0]);
+    area = Math.abs(area) / 2;
 
+    const multiplyer = 12000000000;
+    //arbitrary multiplier to simulate
+    //nominal power calculation
 
     setNominalPower(area * multiplyer);
-  };
 
-  const handleLocateClick = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log("position", position);
-      setLat(position.coords.latitude);
-      setLng(position.coords.longitude);
-    });
+    //set flag when user has drawn to remove
+    //hint text, show nominal power results
+    setHasDrawn(true);
   }
 
+  //handler for address search
   const onSelected = (viewport, item) => {
-    console.log(item);
     setGeoCode(item.place_name);
-    setLng(item.center[0]);
-    setLat(item.center[1]);
+    setCenter(item.center);
   }
 
   const handleMapMove = ({ transform }) => {
-    setLng(transform.center.lng);
-    setLat(transform.center.lat);
+    setCenter(transform.center);
   }
 
-  return (
-    <div className="inner">
-      <h1>Nominal Power Estimator</h1>
-      <div>
+  const handleMapZoom = ({ transform }) => {
+    setZoom(transform.zoom);
+  }
 
-        <p>Enter an address to get started.</p>
-        <form>
-          <label>
-            Enter Address:
-          </label>
+  // WIP: geolocation feature
+  // const handleLocateClick = () => {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     setCenter([position.coords.latitude, position.coords.longitude]);
+  //   });
+  // }
+
+  return (
+    <div className="map__container">
+
+      <Map
+        style='mapbox://styles/mapbox/streets-v11' // eslint-disable-line
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+        center={center}
+        onMoveEnd={handleMapMove}
+        onZoomEnd={handleMapZoom}
+        zoom={[zoom]}
+      >
+        <DrawControl onDrawUpdate={handleDraw} onDrawCreate={handleDraw} />
+      </Map>
+      <div className="calculator__container">
+
+        <h1>Nominal Power Estimator</h1>
+        <label>Address
           <Geocoder
             mapboxApiAccessToken={accessToken}
+            initialInputValue="'1234 Solar Avenue'"
             hideOnSelect={true}
             value={geoCode}
             onSelected={onSelected}
+            updateInputOnSelect={true}
           />
-          <button type="button" onClick={handleLocateClick}>Locate Me</button>
-        </form>
-      </div>
-      <div>
-        <p>Draw a polygon around your estimated solar installation.</p>
-        <Map
-          style='mapbox://styles/mapbox/streets-v11' // eslint-disable-line
-          containerStyle={{
-            height: "30vh",
-            width: "100vw",
-            maxWidth: "400px"
-          }}
-          center={[lng, lat]}
-          zoom={[zoom]}
-          onMoveEnd={handleMapMove}
-        >
-          <DrawControl onDrawCreate={handleDraw} onDrawUpdate={handleDraw} />
-        </Map>
+        </label>
 
-      </div>
-      <div className="nominal-power-result-container">
-        <p>Nominal Power:</p>
-        <p>~{Math.floor(nominalPower)}V</p>
+        <div className="nominal-power-result-container">
+          {!hasDrawn && (<div>
+            <p>Draw a polygon around your estimated solar installation.</p>
+          </div>)}
+          {hasDrawn && (<div>
+            <p>Nominal Power:</p>
+            <p className="nominal-total">~{Math.floor(nominalPower)}V</p>
+          </div>)}
+        </div>
       </div>
     </div>
   );
