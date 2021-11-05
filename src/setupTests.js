1b7314d3a76be2ccde974e24895f743400753ab3
luis <luis@betabrand.com>
2021-11-05 09:30:58 -0700

    update testing, adding mock `map` obj so mapbox-gl to works with jest-dom

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// Mock `map` for so that it can be loaded in tests
+export const mockMapOn = jest.fn();
+export const mockMapRemove = jest.fn();
+export const mockMapGetCenter = jest.fn();
+export const mockMapGetZoom = jest.fn();
+export const mockMapGetBearing = jest.fn();
+export const mockMapGetPitch = jest.fn();
+
+jest.mock('mapbox-gl', () => ({
+  Map: function () {
+    this.on = mockMapOn;
+    this.remove = mockMapRemove;
+    this.getCenter = mockMapGetCenter;
+    this.getZoom = mockMapGetZoom   
+    this.getBearing = mockMapGetBearing;
+    this.getPitch = mockMapGetPitch;
+  }
+}));
